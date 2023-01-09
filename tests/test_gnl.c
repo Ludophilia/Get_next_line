a2821f1469098f172a9e6fd33551873239094295
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2023-01-09 12:23:31 +0100

    Completed the series of tests testing how my implementation of gnl handles files with no newline

--- tests/test_gnl.c
+++ tests/test_gnl.c
@@ -6,7 +6,7 @@
 /*   By: jgermany <nyaritakunai@outlook.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 13:04:17 by jgermany          #+#    #+#             */
-/*   Updated: 2023/01/08 19:53:42 by jgermany         ###   ########.fr       */
+/*   Updated: 2023/01/09 12:19:41 by jgermany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <assert.h>
 #include <string.h>
 
-void	test_line(int fd, char *expect)
+static void	test_line(int fd, char *expect)
 {
 	char	*line;
 	int		test;
@@ -28,7 +28,7 @@ void	test_line(int fd, char *expect)
 	free(line);
 }
 
-void	gnl_testcase(char *name, char *path,  int lines, char *expect[])
+static void	gnl_testcase(char *name, char *path,  int lines, char *expect[])
 {
 	int		fd;
 	int		i;
@@ -41,23 +41,27 @@ void	gnl_testcase(char *name, char *path,  int lines, char *expect[])
 	close(fd);
 }
 
-// GNL return the line from fd (with a \n if it contains one) as a char *
-// Repeated calls to your gnl should let you read the fd one line at a time
-// or NULL/(char *)0 in case of error.
-int	main(void)
+static void	happy_path_series(int doit)
 {
-	printf("get_next_line with BUFFER_SIZE=%i\n", BUFFER_SIZE);
-	// gnl_testcase(
-	// 	"Happy Path (1)",
-	// 	"tests/testfile_hp1.txt",
-	// 	4,
-	// 	(char *[]){
-	// 		"Hello everyone,\n",
-	// 		"Glad to see you all again in such good shape :)",
-	// 		(char *)0,
-	// 		(char *)0
-	// 	}
-	// );
+	if (!doit)
+		return ;
+	gnl_testcase(
+		"Happy Path (1)",
+		"tests/testfile_hp1.txt",
+		4,
+		(char *[]){
+			"Hello everyone,\n",
+			"Glad to see you all again in such good shape :)",
+			(char *)0,
+			(char *)0
+		}
+	);
+}
+
+static void no_newline_series(int doit)
+{
+	if (!doit)
+		return ;
 	gnl_testcase(
 		"No newline (1)",
 		"tests/testfile_nl1.txt",
@@ -67,30 +71,99 @@ int	main(void)
 			(char *)0
 		}
 	);
-	// gnl_testcase(
-	// 	"No newline (1)",
-	// 	"tests/testfile_nl2.txt",
-	// 	6,
-	// 	(char *[]){
-	// 		"z",
-	// 		(char *)0,
-	// 		(char *)0,
-	// 		(char *)0,
-	// 		(char *)0,
-	// 		(char *)0
-	// 	}
-	// );
-	// gnl_testcase(
-	// 	"No newline (4)",
-	// 	"tests/testfile_nl4.txt",
-	// 	3,
-	// 	(char *[]){
-	// 		"this file will contain a decent amount of characters,"
-	// 		" but on a single line only, so let's get started. Do you"
-	// 		" know what a is? I don't know either... I didn't even"
-	// 		" correctly described it!",
-	// 		(char *)0,
-	// 		(char *)0
-	// 	}
-	// );
+	gnl_testcase(
+		"No newline (2)",
+		"tests/testfile_nl2.txt",
+		6,
+		(char *[]){
+			"z",
+			(char *)0,
+			(char *)0,
+			(char *)0,
+			(char *)0,
+			(char *)0
+		}
+	);
+	gnl_testcase(
+		"No newline (3)",
+		"tests/testfile_nl3.txt",
+		6,
+		(char *[]){
+			"this file still has no newline but is a little more verbose :)",
+			(char *)0,
+			(char *)0,
+			(char *)0,
+			(char *)0,
+			(char *)0
+		}
+	);
+	gnl_testcase(
+		"No newline (4)",
+		"tests/testfile_nl4.txt",
+		3,
+		(char *[]){
+			"this file will contain a decent amount of characters,"
+			" but on a single line only, so let's get started. Do you"
+			" know what a is? I don't know either... I didn't even"
+			" correctly described it!",
+			(char *)0,
+			(char *)0
+		}
+	);
+	gnl_testcase(
+		"No newline (5)",
+		"tests/testfile_nl5.txt",
+		3,
+		(char *[]){
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "
+			"eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+			"Massa id neque aliquam vestibulum. Sit amet massa vitae tortor. "
+			"In pellentesque massa placerat duis ultricies. Velit euismod in "
+			"pellentesque massa. Egestas congue quisque egestas diam in arcu "
+			"cursus. Tortor id aliquet lectus proin nibh nisl condimentum "
+			"id. Pulvinar mattis nunc sed blandit. Blandit massa enim nec "
+			"dui nunc mattis. Dui nunc mattis enim ut tellus elementum "
+			"sagittis vitae. Volutpat ac tincidunt vitae semper quis lectus "
+			"nulla at volutpat. Velit scelerisque in dictum non. Ut faucibus "
+			"pulvinar elementum integer enim neque. Sociis natoque penatibus "
+			"et magnis dis parturient montes nascetur ridiculus. Pharetra "
+			"vel turpis nunc eget lorem. Leo urna molestie at elementum. "
+			"Arcu bibendum at varius vel pharetra vel turpis nunc. Justo "
+			"donec enim diam vulputate ut pharetra sit amet. Non odio "
+			"euismod lacinia at quis risus sed vulputate. Fermentum odio eu "
+			"feugiat pretium. Arcu dictum varius duis at consectetur. Et "
+			"ultrices neque ornare aenean euismod elementum. Nisi est sit "
+			"amet facilisis magna etiam tempor. Scelerisque fermentum dui "
+			"faucibus in ornare quam viverra. Faucibus ornare suspendisse "
+			"sed nisi. Sed vulputate odio ut enim blandit volutpat maecenas. "
+			"Nascetur ridiculus mus mauris vitae ultricies leo integer "
+			"malesuada. Vulputate eu scelerisque felis imperdiet proin "
+			"fermentum. Amet facilisis magna etiam tempor. Vel quam "
+			"elementum pulvinar etiam. Pellentesque pulvinar pellentesque "
+			"habitant morbi tristique senectus et netus. Augue neque gravida "
+			"in fermentum et sollicitudin ac orci phasellus. Lacus sed "
+			"viverra tellus in hac habitasse platea dictumst. Quis hendrerit "
+			"dolor magna eget.Tincidunt augue interdum velit euismod. Urna "
+			"duis convallis convallis tellus id interdum velit. Nam libero "
+			"justo laoreet sit amet cursus sit amet dictum. Lorem ipsum "
+			"dolor sit amet consectetur adipiscing elit pellentesque. Proin "
+			"nibh nisl condimentum id venenatis a condimentum vitae. Nibh "
+			"ipsum consequat nisl vel pretium. Morbi enim nunc faucibus a "
+			"pellentesque sit amet porttitor eget. Nunc aliquet bibendum "
+			"enim facilisis gravida neque. Adipiscing enim eu turpis egestas "
+			"pretium aenean pharetra. Mattis enim ut tellus elementum "
+			"sagittis. Mauris ultrices eros in cursus turpis massa tincidunt "
+			"dui. Aliquam ultrices sagittis orci a scelerisque purus. Quam "
+			"lacus suspendisse faucibus interdum posuere lorem ipsum dolor.",
+			(char *)0,
+			(char *)0
+		}
+	);
+}
+
+int	main(void)
+{
+	printf("get_next_line with BUFFER_SIZE=%i\n", BUFFER_SIZE);
+	happy_path_series(0);
+	no_newline_series(1);
 }
--- tests/test_suite.sh
+++ tests/test_suite.sh
@@ -3,8 +3,8 @@
 SIZE=1 make
 SIZE=10 make
 SIZE=16 make
-SIZE=42 make
-SIZE=999 make
+# SIZE=42 make
+# SIZE=999 make
 SIZE=1024 make
-SIZE=20230 make
+# SIZE=20230 make
 SIZE=100000000 make
