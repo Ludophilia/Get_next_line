05d629e217b6931fee199395947f360ac656a7b3
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2024-11-24 19:00:47 +0100

    Writing the code to extract a line from the stash

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 13:51:33 by jegerman          #+#    #+#             */
-/*   Updated: 2024/11/24 18:41:13 by jegerman         ###   ########.fr       */
+/*   Updated: 2024/11/24 18:59:34 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,31 +41,43 @@ ssize_t	update_stash(int fd, char **stash)
 	{
 		old_stash = *stash;
 		*stash = ft_strjoin(old_stash, buffer);
-		free(old_stash); // Inconsistent?
 		if (*stash == NULL)
 			return (-1);
+		free(old_stash);
 	}
 	return (bytesread);
 }
 
+char	*extract_line(char **stash)
+{
+	
+}
+
+// char	*ft_substr(char const *s, unsigned int start, size_t len)
+
+// What it should do, because iForgot?
+//	- read from a fd
+//		- store the result into a buffer 
+// 	- update the stash with the buffer
+
+// - return the line (if '\n' or EOF)
 char	*get_next_line(int fd)
 {
 	static char		*stash;
+	char			*line;
 	ssize_t			bytesread;
 
 	if (fd < 0)
 		return (NULL);
-	bytesread = update_stash(int fd, char **stash, )
-	while (bytesread > 0 && ft_strchr(sta) == NULL)
-	// What it should do, because iForgot?
-	//	- read from a fd
-	//		- store the result into a buffer 
-	// 	- update the stash with the buffer
-
-	// - return the line (if '\n' or EOF)
-
-	bytesread = ;
-
-	
-	return (NULL); // line
+	// stash = NULL // DOUBT
+	bytesread = update_stash(fd, &stash);
+	while (bytesread > 0 && get_char_pos(stash, '\n') == -1)
+		bytesread = update_stash(fd, &stash);
+	if (bytesread == -1)
+	{
+		free(stash);
+		return (NULL);
+	}
+	line = extract_line(char **stash);
+	return (line);
 }
